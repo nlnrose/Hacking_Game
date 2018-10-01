7078db0af8893a9bc0dcf9bc68ad08a3d36a5840
nlnrose <nln_rose@comcast.net>
2018-09-30 21:22:47 -0600

    Unit tests are now running, though with an error.

--- src/generator_test.py
+++ src/generator_test.py
@@ -1,6 +1,6 @@
 import unittest
 from generator import generator
 class generator_test(unittest.TestCase):
-    def generate_list_test(self):
+    def test_generate_list(self):
         self.assertEqual(generator.generate_list().length, 3)
         pass
